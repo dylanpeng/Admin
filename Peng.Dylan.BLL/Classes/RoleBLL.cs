c5893389f05c2f82716c1f6da507ce2eb0b84d25
peng_dylan <peng_dylan@sina.com>
2017-11-17 18:54:22 +0800

    2017-11-17： 添加角色

---
+++ Peng.Dylan.BLL/Classes/RoleBLL.cs
@@ -0,0 +1,32 @@
+﻿using Peng.Dylan.Common;
+using Peng.Dylan.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peng.Dylan.BLL.Classes
+{
+    public class RoleBLL
+    {
+        /// <summary>
+        /// 添加账户
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        public static int AddRole(RoleEntity role)
+        {
+            try
+            {
+                var dao = UnityInstance.GetInstanceDAL<IRoleDao>();
+                var result = dao.AddRole(role);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+        }
+    }
+}
